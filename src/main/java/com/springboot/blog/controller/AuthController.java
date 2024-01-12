639f0e3ce15e7c4ce910455b92cd5776b8a2bdac
RameshMF <ramesh24fadatare@gmail.com>
2024-01-12 16:05:43 +0530

    Removed deprecated JWT methods from JwtTokenProvider

--- src/main/java/com/springboot/blog/controller/AuthController.java
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private AuthService authService;
--- src/main/java/com/springboot/blog/security/JwtTokenProvider.java
+++ src/main/java/com/springboot/blog/security/JwtTokenProvider.java
@@ -1,7 +1,10 @@
 package com.springboot.blog.security;
 
 import com.springboot.blog.exception.BlogAPIException;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
@@ -23,6 +27,7 @@ public class JwtTokenProvider {
 
     // generate JWT token
     public String generateToken(Authentication authentication){
+
         String username = authentication.getName();
 
         Date currentDate = new Date();
@@ -30,47 +35,46 @@ public class JwtTokenProvider {
         Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
 
         String token = Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(expireDate)
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(expireDate)
                 .signWith(key())
                 .compact();
+
         return token;
     }
 
     private Key key(){
-        return Keys.hmacShaKeyFor(
-                Decoders.BASE64.decode(jwtSecret)
-        );
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
-    // get username from Jwt token
+    // get username from JWT token
     public String getUsername(String token){
-        Claims claims = Jwts.parser()
-                .setSigningKey(key())
+
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
-        String username = claims.getSubject();
-        return username;
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
     }
 
-    // validate Jwt token
+    // validate JWT token
     public boolean validateToken(String token){
         try{
             Jwts.parser()
-                    .setSigningKey(key())
+                    .verifyWith((SecretKey) key())
                     .build()
                     .parse(token);
             return true;
-        } catch (MalformedJwtException ex) {
-            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT token");
-        } catch (ExpiredJwtException ex) {
+        }catch (MalformedJwtException malformedJwtException){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT Token");
+        }catch (ExpiredJwtException expiredJwtException){
             throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Expired JWT token");
-        } catch (UnsupportedJwtException ex) {
+        }catch (UnsupportedJwtException unsupportedJwtException){
             throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Unsupported JWT token");
-        } catch (IllegalArgumentException ex) {
-            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "JWT claims string is empty.");
+        }catch (IllegalArgumentException illegalArgumentException){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Jwt claims string is null or empty");
         }
     }
 }
